ae52962a4a3e4aaaec820e3ac530418a9e2138ce
NazaretMelquiades <nazaretmelquiades@hotmail.com>
2025-09-05 13:20:08 +0200

    FirstCommit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import List from "./List"
+
+const Main = () => {
+  return (
+    <main>
+      Main
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Main/TodoList/TodoItem/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Item.css'
+
+const TodoItem = ({ data, remove }) => {
+  const { title, description, isDone } = data;
+
+  return <article>
+    <h4>{title}</h4>
+    <p>{description}</p>
+    <label htmlFor="done">Terminado:</label>
+    <input id="done" type="checkbox" />
+    <button onClick={remove}>Borrar</button>
+  </article>
+};
+
+export default TodoItem;
